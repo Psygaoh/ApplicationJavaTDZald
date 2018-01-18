0e36ba60bd5b3145f427a62ee8460cc39b40d727
DESKTOP-L5TSQHQ\etudiant28 <florian.ville63@gmail.com>
2018-01-18 16:58:56 +0100

    edit et test sur retour home
    fail

--- src/GUI/HelloWorldWindows.java
+++ src/GUI/HelloWorldWindows.java
@@ -1,16 +1,31 @@
 package GUI;
 
+import Application.HelloWorld;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class HelloWorldWindows {
+public class HelloWorldWindows extends JFrame implements ActionListener {
 
-    public static JPanel buildEx1Content(){
+    public static   JPanel buildEx1Content(){
 
         JPanel container = new JPanel();
         JButton boutonRetour = new JButton("retour");
 
 
         container.add(boutonRetour);
+
+        boutonRetour.addActionListener(HelloWorldWindows::retourHome() );
+
         return container;
+    }
+
+    private static void retourHome(ActionEvent actionEvent) {
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
 }
